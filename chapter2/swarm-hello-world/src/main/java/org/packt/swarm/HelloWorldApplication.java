fb5ebe122ece82d92c23653444ccd289ad9f303d
packtsurabhi <33089349+packtsurabhi@users.noreply.github.com>
2018-03-29 17:35:02 +0530

    Delete HelloWorldApplication.java

--- chapter2/swarm-hello-world/src/main/java/org/packt/swarm/HelloWorldApplication.java
+++
@@ -1,8 +0,0 @@
-package org.packt.swarm;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class HelloWorldApplication extends Application {
-}
